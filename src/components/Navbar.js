c6a1b2f639483e6786ec1f8e9a906d1603b0fc5f
Student <example@example.com>
2019-09-26 10:21:01 -0700

    router sent props automatically to other component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return(
@@ -8,8 +8,8 @@ const Navbar = () => {
         <a className="brand-logo">Poke'Times</a>
         <ul className="right">
           <li><Link to="/">Home</Link></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
     </nav>
