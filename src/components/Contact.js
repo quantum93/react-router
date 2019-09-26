1e6242a68fd771adb14fd43eb3739741416c158c
Student <example@example.com>
2019-09-26 10:25:15 -0700

    It shows the programmatic redirects using setTimeout for contact/about pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Contact = (props) => {
-  console.log(props)
+  // console.log(props)
+  setTimeout(() => {props.history.push('/about')}, 2000);
+
   return(
     <div className="container">
       <h4 className="center">Contact</h4>
