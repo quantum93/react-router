7905a64843d28cdeb9e6df016d891cc536a61990
Student <example@example.com>
2019-09-26 11:48:12 -0700

    route parameter with componentDidMount

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 
 class Post extends Component {
+  state = {id: null};
+
+  componentDidMount() {
+    // console.log(this.props);
+    let id = this.props.match.params.post_id;
+    this.setState({id: id});
+  }
+
   render() {
     return(
       <div className="container">
-        <h4>route param</h4>
+        <h4>{this.state.id}</h4>
       </div>
     )
   }
