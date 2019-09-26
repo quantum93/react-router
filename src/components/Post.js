4141747f63d405bad726914938f4e47bcf2b8129
Student <example@example.com>
2019-09-26 12:02:22 -0700

    It works for route parameter

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Post extends Component {
-  state = {id: null};
+  // state = {id: null};
+  state = {post: null};
 
   componentDidMount() {
     // console.log(this.props);
     let id = this.props.match.params.post_id;
-    this.setState({id: id});
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    .then(res => {
+      this.setState({post: res.data})
+      // console.log(res)
+    })
+    // this.setState({id: id});
   }
 
   render() {
+
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    )
+
     return(
       <div className="container">
-        <h4>{this.state.id}</h4>
+        { post }
       </div>
     )
   }
