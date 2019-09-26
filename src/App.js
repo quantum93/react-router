4ce9223358870ecf80c355a4d0bbdb146aadc158
Student <example@example.com>
2019-09-26 13:08:32 -0700

    It enables to view only one page using Switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -12,10 +12,12 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
+          <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route path='/:post_id' component={Post} />
+          </Switch>
         </div>
       </Router>
     );
